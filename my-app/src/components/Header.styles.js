365c25f4f4ed646d908f15844802a7fdfe99a45c
idiatou <idiatoudiallo1741@gmail.com>
2021-07-15 16:05:22 -0400

    component

---
+++ my-app/src/components/Header.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+background: var(--darkGrey);
+padding:0 20px;
+`;
+
+export const Content = styled.div`
+display: flex;
+`;
+
+export const LogoImg = styled.img``;
+export const TMDBLogoImg = styled.img``;
